cbd1d16af4f49cec0a7f53f563b1f0b883265c28
Raju Ranjan Yadav <62349995+309666@users.noreply.github.com>
2022-11-13 07:50:32 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// console.log("Raju Ranjan Yadav");    // first Exercise 
+
+// var readlineSync = require('readline-sync');
+// var userName = readlineSync.question('What is your name?');    // 2nd Exercise 
+// console.log(userName);
+
+// var welcomeMessage = "Welcome " + userName;   // string concatenation     ( 3rd Exercise)
+// console.log(welcomeMessage);
+
+//  do it all together ( 4th Exercise)
+
+
+
